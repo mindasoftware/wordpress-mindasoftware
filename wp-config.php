ead730d3224269263f12469264023b42c1f175be
mindasoftware <piotr-minda@wp.pl>
2018-07-15 13:42:13 +0200

    Ustawienia bazy danych na 192.168.10.102 - baza danych dev_mindasoftware

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_mindasoftware');
+define('DB_NAME', 'dev_mindasoftware');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wp_mindasoftware');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'J96xIr17DVbwgpXw');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.10.102');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
